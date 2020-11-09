c3e4ff5b30091b442596e4596074d46753ce937c
isaccanedo <isaccanedo@gmail.com>
2020-11-08 21:51:11 -0300

    Initial commit

---
+++ src/main/java/br/com/isaccanedo/qrcode/QRCodeGenerator.java
@@ -0,0 +1,48 @@
+package br.com.isaccanedo.qrcode;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/**
+ * @author Isac Canedo
+ */
+public class QRCodeGenerator {
+    private static final String QR_CODE_IMAGE_PATH = "./MyQRCode.png";
+
+    private static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    private byte[] getQRCodeImageByteArray(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+        byte[] pngData = pngOutputStream.toByteArray();
+        return pngData;
+    }
+
+    public static void main(String[] args) {
+        try {
+            generateQRCodeImage("Este é o meu primeiro QR Code", 350, 350, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Não foi possível gerar o código QR, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Não foi possível gerar o código QR, IOException :: " + e.getMessage());
+        }
+    }
+}
